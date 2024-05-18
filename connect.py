7a77f4caf31a3d074356da211836b2eca4208dcb
bruc3600 <bruc3600@mylaurier.ca>
2024-05-18 15:14:10 -0400

    Update connect.py

--- connect.py
+++ connect.py
@@ -1,6 +1,6 @@
 from pymongo.mongo_client import MongoClient
 from pymongo.server_api import ServerApi
-uri = "mongodb+srv://bruceandrew11:<password>@cluster0.m6ptqli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+uri = "mongodb+srv://bruceandrew11:HackathonDBTest@cluster0.m6ptqli.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
 # Create a new client and connect to the server
 client = MongoClient(uri, server_api=ServerApi('1'))
 # Send a ping to confirm a successful connection
